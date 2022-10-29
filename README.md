1313174a19ccf771a76f054006f165a3e391aed1
Johny Krupka <106292161+krupkajohny@users.noreply.github.com>
2022-10-29 15:49:06 +1100

    readme update

--- README.md
+++ README.md
@@ -2,8 +2,21 @@
 
 # Live version: https://krupkajohny.github.io/sign-up-form/
 
-## HTML - exercising:
-
 ## CSS - exercising:
 
+- global reset
+- reusable grid / flexbox classes
+- absolute positioning
+- font size clamp function
+- media queries
+
 ## JavaScript - exercising:
+
+Basic dom manipulation, functions and program logic
+
+- selecting elements
+- getting values from inputs
+- basic string methods (.trim, .toUpperCase, .slice, etc.)
+- first encounter with the Regex
+- event listeners
+- if else statements
--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 /****** STRICT mode ON *******/
-"use strict";
+'use strict';
 
 /****** Selecting DOM elements *******/
-const form = document.querySelector(".signUpForm");
-const elButton = document.querySelector(".submitBtn");
+const form = document.querySelector('.signUpForm');
+const elButton = document.querySelector('.submitBtn');
 
 //inputs
-const inputFirstName = document.querySelector(".firstName");
-const inputLastName = document.querySelector(".lastName");
-const inputEmail = document.querySelector(".email");
-const inputPhone = document.querySelector(".phoneNumber");
-const inputFirstPassword = document.querySelector(".firstPassword");
-const inputConfirmPassword = document.querySelector(".confirmPassword");
+const inputFirstName = document.querySelector('.firstName');
+const inputLastName = document.querySelector('.lastName');
+const inputEmail = document.querySelector('.email');
+const inputPhone = document.querySelector('.phoneNumber');
+const inputFirstPassword = document.querySelector('.firstPassword');
+const inputConfirmPassword = document.querySelector('.confirmPassword');
 
 // text messages
-const elFirstNameMsg = document.querySelector(".firstNameMsg");
-const elLastNameMsg = document.querySelector(".lastNameMsg");
-const elEmailMsg = document.querySelector(".emailMsg");
-const elPhoneNumberMsg = document.querySelector(".phoneNumberMsg");
-const elFirstPasswordMsg = document.querySelector(".firstPasswordMsg");
-const elConfirmPasswordMsg = document.querySelector(".confirmPasswordMsg");
+const elFirstNameMsg = document.querySelector('.firstNameMsg');
+const elLastNameMsg = document.querySelector('.lastNameMsg');
+const elEmailMsg = document.querySelector('.emailMsg');
+const elPhoneNumberMsg = document.querySelector('.phoneNumberMsg');
+const elFirstPasswordMsg = document.querySelector('.firstPasswordMsg');
+const elConfirmPasswordMsg = document.querySelector('.confirmPasswordMsg');
 
 /****** Data *******/
 // validSymbols (phone, email etc.)
@@ -38,12 +38,10 @@ const emptyInputCheck = (input) => input.value.trim().length > 0;
 // input validation including special symbols checking if necessary
 const inputValidation = (symbols = false, input, msgEl, errMsgStartStr) => {
   // error message initial reset
-  msgEl.innerText = "";
+  msgEl.innerText = '';
 
   // error message string for empty input
-  const errRequiredStr = `${
-    errMsgStartStr.slice(0, 1).toUpperCase() + errMsgStartStr.slice(1)
-  } is required!`;
+  const errRequiredStr = `${errMsgStartStr.slice(0, 1).toUpperCase() + errMsgStartStr.slice(1)} is required!`;
 
   // error message string for forbidden symbols
   const errFormatStr = `Incorrect ${errMsgStartStr} format!`;
@@ -52,8 +50,7 @@ const inputValidation = (symbols = false, input, msgEl, errMsgStartStr) => {
   if (!emptyInputCheck(input)) return displayMsg(msgEl, errRequiredStr);
 
   // cutting spaces before/after and checking if symbols mach => if not display error
-  if (symbols && !input.value.trim().match(symbols))
-    return displayMsg(msgEl, errFormatStr);
+  if (symbols && !input.value.trim().match(symbols)) return displayMsg(msgEl, errFormatStr);
 };
 
 // if password is too short => display error
@@ -73,51 +70,36 @@ const passConfirmCheck = (inputPass, msgEl, inputConfirm) => {
   const passNotMatchErrStr = `Passwords don't match`;
 
   // if passwords don't match => display error
-  if (!arrMsgIs && confirmIsNotPass)
-    return displayMsg(msgEl, passNotMatchErrStr);
+  if (!arrMsgIs && confirmIsNotPass) return displayMsg(msgEl, passNotMatchErrStr);
 };
 
 /****** Event listeners *******/
 
 // form inputValidation on the button submit (Create Account)
-elButton.addEventListener("click", (e) => {
+elButton.addEventListener('click', (e) => {
   e.preventDefault();
 
   // inputFirstName inputValidation (empty input)
-  inputValidation(undefined, inputFirstName, elFirstNameMsg, "first name");
+  inputValidation(undefined, inputFirstName, elFirstNameMsg, 'first name');
 
   // inputLastName inputValidation (empty input)
-  inputValidation(undefined, inputLastName, elLastNameMsg, "last name");
+  inputValidation(undefined, inputLastName, elLastNameMsg, 'last name');
 
   // inputEmail inputValidation (empty input) incl. symbols checking
-  inputValidation(validSymbols.email, inputEmail, elEmailMsg, "email");
+  inputValidation(validSymbols.email, inputEmail, elEmailMsg, 'email');
 
   // inputPhone inputValidation incl. (empty input) symbols checking
-  inputValidation(validSymbols.phone, inputPhone, elPhoneNumberMsg, "phone");
+  inputValidation(validSymbols.phone, inputPhone, elPhoneNumberMsg, 'phone');
 
   // inputFirstPassword inputValidation
   // (empty input)
-  inputValidation(
-    undefined,
-    inputFirstPassword,
-    elFirstPasswordMsg,
-    "password"
-  );
+  inputValidation(undefined, inputFirstPassword, elFirstPasswordMsg, 'password');
   // short password
   passLengthCheck(inputFirstPassword, elFirstPasswordMsg, 6);
 
   // inputConfirmPassword inputValidation
   // (empty input)
-  inputValidation(
-    undefined,
-    inputConfirmPassword,
-    elConfirmPasswordMsg,
-    "password"
-  );
+  inputValidation(undefined, inputConfirmPassword, elConfirmPasswordMsg, 'password');
   // passwords don't match
-  passConfirmCheck(
-    inputFirstPassword,
-    elConfirmPasswordMsg,
-    inputConfirmPassword
-  );
+  passConfirmCheck(inputFirstPassword, elConfirmPasswordMsg, inputConfirmPassword);
 });
